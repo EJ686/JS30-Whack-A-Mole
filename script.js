6a21eee829cf59cfa3b52bdd726c1bc1b6015b32
Edgar Juarez <ejjt.686@gmail.com>
2017-06-26 17:46:47 -0700

    Random hole function wont select the same hole more than once in a row

--- script.js
+++ script.js
@@ -1,13 +1,20 @@
 const holes = document.querySelectorAll('.hole');
 const scoreBoard = document.querySelector('.score');
 const moles = document.querySelectorAll('.mole');
+let lastHole;
 
-function randTime(min, max) {
+
+function randomTime(min, max) {
     return Math.round(Math.random() * (max - min) + min);
 }
 
 function randomHole(holes) {
     const idx = Math.floor(Math.random() * holes.length);
     const hole = holes[idx];
-    console.log(hole)
+    if(hole === lastHole) {
+        console.log('not again');
+        return randomHole(holes);
+    }
+    lastHole = hole;
+    return hole;
 }
